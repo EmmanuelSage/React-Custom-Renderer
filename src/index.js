fccb8b94cf108b2b52689154a1064716aec9a085
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-10-17 11:15:00 +0100

    setup project for cusrend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Text = props => {
+  return <p className={props.className}>{props.content}</p>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Text className="hello-class" content="Hello" />
+      <span style={{color:'blue'}}>World</span>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
